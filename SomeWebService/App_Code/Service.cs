0c4422a23c7be668bc4284e80e15e4967769a387
Ларионов Константин Александрович <l-k-a@outlook.com>
2017-03-25 14:44:56 +0300

    create web service

---
+++ SomeWebService/App_Code/Service.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+[WebService(Namespace = "http://localhost/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.None)]
+// Чтобы разрешить вызывать веб-службу из скрипта с помощью ASP.NET AJAX, раскомментируйте следующую строку.
+// [System.Web.Script.Services.ScriptService]
+
+public class Service : System.Web.Services.WebService
+{
+    public Service()
+    {
+
+        //Раскомментируйте следующую строку в случае использования сконструированных компонентов 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod(MessageName = "GetSettings")]
+    public List<Setting> GetSettings(string searchField)
+    {
+        var keys = searchField.Split(' ');
+        var list = new List<Setting>();
+        foreach (var key in keys)
+        {
+            list.Add(new Setting { Key = key, Value = "SomeValue" });
+        }
+        return list;
+    }
+
+    [WebMethod(MessageName = "GetSettingsWithoutParams")]
+    public string GetSettings()
+    {
+        return "default_settings";
+    }
+
+}
---
+++ SomeWebService/App_Code/Setting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Сводное описание для Setting
+/// </summary>
+public class Setting
+{
+    public string Key { get; set; }
+    public string Value { get; set; }
+}
